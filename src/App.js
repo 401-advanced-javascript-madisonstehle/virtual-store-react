cda5e53e9acb2f0391756ad63555a32128afe812
Madison Stehle <mnstehle3@gmail.com>
2020-07-10 11:02:01 -0700

    added header and footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store.js';
 
-import Categories from './components/Categories.js'
+import Header from './components/Header.js';
+import Categories from './components/Categories.js';
 import Products from './components/Products.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>Virtual Store</h1>
+        <Header />
         <Categories />
         <Products />
+        <Footer />
       </div>
     </Provider>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h5>&copy; 2020 Madison Stehle</h5>
+    </footer>
+  );
+}
+
+export default Footer;
